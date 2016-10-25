15999148f22b05f0d759cddf99fa914c4f7bbcc1
DimuthuLakmal <kjtdimuthu@gmail.com>
2016-10-25 20:01:52 +0530

    ticket booking is added

--- routes/test.js
+++ routes/test.js
@@ -11,7 +11,7 @@ router.get('/', function (req, res, next) {
     content = new helper.Content("text/plain", "and easy to do anywhere, even with Node.js")
     mail = new helper.Mail(from_email, subject, to_email, content)
 
-    var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+    var sg = require('sendgrid')('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');
     var request = sg.emptyRequest({
         method: 'POST',
         path: '/v3/mail/send',
